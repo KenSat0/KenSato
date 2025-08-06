57b0aeb1b6e31cc89f9eabd61507a0d6ae4e9a7e
Ken Sato <leonardokss1@gmail.com>
2025-08-06 14:44:47 -0300

    ATP 2

--- ATP 2/Codigos/aula1_a.cpp
+++ ATP 2/Codigos/aula1_a.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//definição de tipo Resgistro com as váriaveis necessárias
+
 typedef struct
 {
     char nome[50];
@@ -13,26 +15,45 @@ typedef struct
 
 } Registro;
 
+//criação de vetor tipo Registro com tamanho 20, pois o Spa recebe no maximo 20 hospedes
+
 Registro *hospedes[20];
+
+//cria uma variavel para acompanhar a quantiade de hospedes
 
 int qnt_hospedes = 0;
 
+//função cadastro
+
 int cadastro()
 {
+
+    //laço que repete infinitamente, parando apenas quando algum Return é chamado
+
     while (true)
     {
         int ficha = 0; 
         int dias, cal; 
         float peso_ini;
 
+        //scan na entrada de numero da ficha, -1 fecha a função
+
         printf("\nNumero da ficha (-1 para terminar): ");
         scanf("%d", &ficha);
 
-        if(ficha<1)
+        //fecha a função se ficha == -1 (precisa estar antes da verificação de validade do numero)
+
+        if (ficha == -1) return 1;
+
+        //verifica se o numero é valido para uma ficha -> influencia o indice do vetor
+
+        if(ficha<1 || ficha>=20)
         {
             printf("\nNumero inválido de ficha");
             return 2;
         }
+
+        //verifica se há espaço vago no spa
 
         if (qnt_hospedes >= 20)
         {
@@ -40,21 +61,15 @@ int cadastro()
             return 1;
         }
 
-        if (ficha == -1) return 1;
+        //se a ficha ja existe (se é diferente de nula)
 
         if(hospedes[ficha-1] != NULL)
         {
-            if(hospedes[ficha-1]->alta==1)
-            {
-                printf("\nEssa ficha ja recebeu alta");
-                return 2;
-            }
-            else
-            {
-                printf("\nEssa ficha esta em uso e ainda nao teve alta");
-                return 2;
-            }
+            printf("Ficha ja existe");
+            return 2;
         }
+
+        //aloca memoria no vetor
 
         hospedes[ficha-1] = (Registro*)malloc(sizeof(Registro));
         if (hospedes[ficha-1] == NULL)
@@ -62,25 +77,41 @@ int cadastro()
             printf("\nErro ao alocar memoria\n");
         }
 
+        //da entrada na ficha como sem alta
+
+        hospedes[ficha-1]->alta = 0;
+
+        //registra o numero da ficha
+
         hospedes[ficha-1]->numero_de_ficha = ficha;
+
+        //registra nome (direto na variavel do vetor hospedes)
 
         printf("\nNome: ");
         scanf("%s", hospedes[ficha-1]->nome);
+
+        //registra peso inicial
 
         printf("\nPeso ao entrar: ");
         scanf("%f", &peso_ini);
         hospedes[ficha-1]->peso_inicial = peso_ini;
 
+        //registra dias
+
         printf("\nDias: ");
         scanf("%d", &dias);
-        if (dias > 30)
+        if (dias > 30) //verifica se a quantiade de dias é válida
         {
             printf("\nQuantidade de dias maxima excedida");
             return 0;
         }
         hospedes[ficha-1]->dias = dias;
 
+        //aloca memoria no vetor calorias para cada dia
+
         hospedes[ficha-1]->calorias = (int*)malloc(dias * sizeof(int));
+
+        //registra calorias por dia
 
         for (int i = 0; i<dias; i++)
         {
@@ -88,6 +119,8 @@ int cadastro()
             scanf("%d",  &cal);
             hospedes[ficha-1]->calorias[i] = cal;
         }
+
+        //imprime os valores registrados (sem peso final ou alta)
     
         printf("\n============");
         printf("\nFicha n: %d", hospedes[ficha-1]->numero_de_ficha);
@@ -104,40 +137,59 @@ int cadastro()
         }
         printf("\n============\n");
 
+        //adiciona 1 na quantidade de hospedes
+
         qnt_hospedes++;
-        hospedes[ficha-1]->alta = 0;
+
+        //da opção de cadastrar nova ficha
+
+        int opcao = 0;
+        printf("\nCadastrar nova ficha?\n1- Sim\n2- Fechar\n");
+        scanf("%d", &opcao);
+        if(opcao==2) return 1;
     }
     return 0;
 }
 
+//funcção de dar baixa nas fichas
+
 int dar_baixa()
 {
+
+    //laço sempre verdadeiro, mesmo caso do anterior
+
     while(true)
     {
 
         int ficha;
         float peso_fim;
-        printf("\nNumero da ficha: ");
-        scanf("%d", &ficha);
+        printf("\nNumero da ficha: "); 
+        scanf("%d", &ficha); //recebe um numero de ficha para ser acessada
 
-        if (hospedes[ficha-1 != NULL])
+
+        if(hospedes[ficha-1] == NULL || ficha<1 || ficha>=20) //verifica existencia e validade da ficha desejada
         {
-            if (hospedes[ficha-1]->alta==1)
-            {
-                printf("Essa ficha ja recebeu alta");
+                printf("\nA ficha não existeou seu numero é inválido");
                 return 2;
-            }
+        }
+
+        if(hospedes[ficha-1]->alta==1) //verifica se a ficha ja recebeu alta
+        {
+            printf("\nEssa ficha ja recebeu alta");
+                return 2;
         }
 
         printf("Peso ao sair: ");
         scanf("%f", &peso_fim);
-        hospedes[ficha-1]->peso_final = peso_fim;
+        hospedes[ficha-1]->peso_final = peso_fim; //registra peso final
 
         float perda = hospedes[ficha-1]->peso_inicial - hospedes[ficha-1]->peso_final;
-        printf("Peso perdido: %.2f kg\n", perda);
+        printf("Peso perdido: %.2f kg\n", perda); //retorna a perda de peso
 
-        qnt_hospedes--;
-        hospedes[ficha-1]->alta = 1;
+        qnt_hospedes--; //diminui em 1 a ocupação do spa
+        hospedes[ficha-1]->alta = 1; //da alta na ficha
+
+        //imprime a ficha completa
 
         printf("\n============");
         printf("\nFicha n: %d", hospedes[ficha-1]->numero_de_ficha);
@@ -155,6 +207,8 @@ int dar_baixa()
             printf("\nDia %d: %d", i+1, hospedes[ficha-1]->calorias[i]);
         }
         printf("\n============\n");
+
+        //da opção de dar baixa em mais uma ficha
 
         int opcao = 0;
         printf("\nDar nova baixa?\n1- Sim\n2- Fechar\n");
