94f48c277145fe7070c4a118fd283b2fd61fc848
Ken Sato <leonardokss1@gmail.com>
2025-09-15 15:59:10 -0300

    ATP 2

--- ATP 2/Codigos/C e C++/P1/Estudo final/atividade4e5.cpp
+++ ATP 2/Codigos/C e C++/P1/Estudo final/atividade4e5.cpp
@@ -14,6 +14,7 @@ regFunc *listaRFunc;
 
 int cadastro_ordenado(regFunc**atual)
 {
+    //aloca ponteiro temp e verifica se alocou certo
     regFunc *temp = (regFunc*)malloc(sizeof(regFunc));
     if(temp==NULL)
     {
@@ -21,50 +22,79 @@ int cadastro_ordenado(regFunc**atual)
         return 1;
     }
 
+    //os ponteiros atual e anteior servem para marcar o lugar desejado e o anterior dele
     regFunc *p_atual = *atual;
     regFunc *p_anterior = NULL;
 
+    //pede o numero de matricula e verifica validade dele
     printf("\nMatricula: ");
     scanf("%d", &temp->matricula);
     if(temp->matricula < 1 || temp->matricula > 10)
     {
         perror("\nNumero de cadastro invalido");
+        //se for um numero errado, libera temp
         free(temp);
         return 1;
     }
 
+    //pede nome e guarda no temp->nome
     char nome[50];
     printf("\nNome: ");
     scanf("%s", nome);
     strcpy(temp->nome, nome);
 
+    //isso verifica se a lista ta vazia
     if((*atual)==NULL)
     {
+        //prox recebe NULL, ja que temp é o primeiro elemento
         temp->prox = NULL;
+        //atual recebe temp (pense em *atual como "head" da lista, ou seja, endereço do inicio da lista)
         *atual = temp;
     }
     else
     {
+        //enquanto
+            //ponteiro atual não é nulo (ou seja, até o fim da lista)
+            //o numero da matricula de ponteiro atual for menor que o temp que desejo adicionar (ou seja, para o laço quando chegar no ponteiro que é maior q o ponteiro a ser adicionado)
+            //os ponsteiros atual matricula e temp matricula são diferentes (para o ponteiro se houver duplicidade de matricula)
         while(p_atual!=NULL && p_atual->matricula<temp->matricula && p_atual->matricula!=temp->matricula)
         {
+            //se seguir as 3 condições, ponteiro anterior avança um nó
             p_anterior = p_atual;
+            //após isso, ponteiro atual avança um nó
             p_atual = p_atual->prox;
+
+            //isso mantem sempre os dois ponteiros um após o outro (anterior e depois atual)
         }
+        //se
+            //ponteiro atual não é nulo (ou seja, não acabou a lista)
+            //as matriculas são iguais (atual e a que vai adicionar)
         if(p_atual!=NULL && temp->matricula == p_atual->matricula)
         {
+            //erro de matricula duplicada
             perror("\nNumero de matricula repetido");
             free(temp);
             return 1;
         }
+        //se 
+            //ponteiro anterior == NULL (significa que o laço while nunca rodou, ou seja, o novo nó precisa entrar na primeira posição)
+            //(sabemos disso pois anterior é iniciado como NULL, se ele se mantem assim é pq a posição não foi alterada pelo while)
         if(p_anterior==NULL)
         {
+            //temp->prox recebe atual (endereço inicial), se tornando o novo primeiro nó
             temp->prox = *atual;
+            //o endereço de inicio da lista é atualizado para temp
             *atual = temp;
         }
+        //se nenhum caso aconteecr, é pq o novo nó vai entrar ou no meio, ou no fim da lista, em ambos os casos, o algoritmo é o mesmo
         else
         {
+            //o anterior continua sendo menor que o novo, portanto anterior->prox = temp
             p_anterior->prox = temp;
+            //atual é maior que temp (ou nulo, se for o fim da lista), portanto temp->prox = p_atual
             temp->prox = p_atual;
+
+            //esse caso não lida com o caso de lista vazia pois p_anterior->prox não existe se p_anterior == NULL (valor que ele é iniciado)
         }
     }
     return 0;
